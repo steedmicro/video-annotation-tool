a2df92feb848ce9121e9e3fac0b05a8a60c5af2e
steed924 <steed924@outlook.com>
2021-12-02 17:52:14 +0900

    Erase brush tool & brush size control

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ let MAX_DURATION = 100,
     VIDEO_WIDTH * VIDEO_HEIGHT * MAX_DURATION
   ),
   videoFrameIndex = 0,
-  radius = 10,
+  brushRadius = 16,
   timeInterval = 10,
-  isCanvasMousePressed = false;
+  isCanvasMousePressed = false,
+  isShiftKeyPressed = false,
+  BRUSH_CANVAS_WIDTH = 100,
+  BRUSH_CANVAS_HEIGHT = 100,
+  centerX,
+  centerY;
 const MAX_ALPHA = 120,
   BRUSH_THRESHOLD = 20;
 function draw() {
@@ -27,60 +32,87 @@ function draw() {
   }
   // Draw image data to the canvas
   ctx.putImageData(imageData, 0, 0);
+  ctx.beginPath();
+  ctx.fillStyle = isShiftKeyPressed ? "#ffffff77" : "#ff000077";
+  ctx.arc(centerX, centerY, brushRadius, 0, Math.PI * 2, true);
+  ctx.fill();
 }
+function drawBrush() {
+  const canvas = document.getElementById("brushCanvas");
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#ff000077";
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
 
+  ctx.arc(
+    BRUSH_CANVAS_WIDTH / 2,
+    BRUSH_CANVAS_HEIGHT / 2,
+    brushRadius,
+    0,
+    Math.PI * 2,
+    true
+  );
+  ctx.fill();
+}
 $(document).ready(function () {
   const leftVideo = document.querySelector("#right_video");
   const rightVideo = document.querySelector("#right_video");
   rightVideo.addEventListener("loadeddata", (e) => {
-    //MAX_DURATION = Math.floor(rightVideo.duration * FRAMES_PER_SECOND);
     $("#slider").attr("max", MAX_DURATION - 1);
   });
+  $("#canvas").focus();
   $("#canvas").mousedown(function () {
     isCanvasMousePressed = true;
   });
   $("#canvas").mouseup(function () {
     isCanvasMousePressed = false;
   });
-  $("#canvas").keydown(function (e) {
-    console.log(e);
-  });
-  $("#canvas").keyup(function (e) {
-    console.log(e);
+  $(document).on("keyup keydown", function (e) {
+    isShiftKeyPressed = e.shiftKey;
+    draw();
   });
   $("#canvas").mousemove(function (e) {
-    if (!isCanvasMousePressed) return;
-    const centerX = e.offsetX,
-      centerY = e.offsetY;
-    let x, y, t;
+    isShiftKeyPressed = e.shiftKey;
+    (centerX = e.offsetX), (centerY = e.offsetY);
+    if (!isCanvasMousePressed) {
+      draw();
+      return;
+    }
+
+    let x, y, t, d, sign, offset, offsetWithTime;
     for (
       t = videoFrameIndex - timeInterval;
       t <= videoFrameIndex + timeInterval;
       t++
     ) {
       if (t < 0 || t >= MAX_DURATION) continue;
-      for (x = centerX - radius; x <= centerX + radius; x++) {
+      for (x = centerX - brushRadius; x <= centerX + brushRadius; x++) {
         if (x < 0 || x >= VIDEO_WIDTH) continue;
-        for (y = centerY - radius; y <= centerY + radius; y++) {
+        for (y = centerY - brushRadius; y <= centerY + brushRadius; y++) {
           if (y < 0 || y >= VIDEO_HEIGHT) continue;
-          videoAnnotationData[
-            t * VIDEO_WIDTH * VIDEO_HEIGHT + y * VIDEO_WIDTH + x
-          ] = Math.min(
+          d =
             videoAnnotationData[
               t * VIDEO_WIDTH * VIDEO_HEIGHT + y * VIDEO_WIDTH + x
-            ] +
-              ((BRUSH_THRESHOLD -
-                Math.min(
-                  (BRUSH_THRESHOLD *
-                    ((x - centerX) * (x - centerX) +
-                      (y - centerY) * (y - centerY))) /
-                    (radius * radius),
-                  BRUSH_THRESHOLD
-                )) *
-                (timeInterval - Math.abs(t - videoFrameIndex))) /
-                timeInterval,
-            MAX_ALPHA
-          );
+            ];
+          sign = isShiftKeyPressed ? -1 : 1;
+          offset =
+            BRUSH_THRESHOLD *
+            Math.min(
+              1,
+              Math.max(
+                0,
+                1 -
+                  ((x - centerX) * (x - centerX) +
+                    (y - centerY) * (y - centerY)) /
+                    (brushRadius * brushRadius)
+              )
+            );
+          offsetWithTime =
+            (offset * (timeInterval - Math.abs(t - videoFrameIndex))) /
+            timeInterval;
+          videoAnnotationData[
+            t * VIDEO_WIDTH * VIDEO_HEIGHT + y * VIDEO_WIDTH + x
+          ] = Math.max(Math.min(d + sign * offsetWithTime, MAX_ALPHA), 0);
         }
       }
     }
@@ -88,11 +120,16 @@ $(document).ready(function () {
     draw();
   });
   draw();
-
+  drawBrush();
   $("#slider").change(function (e) {
-    videoFrameIndex = e.target.value;
+    videoFrameIndex = parseInt(e.target.value);
     rightVideo.currentTime =
       (videoFrameIndex * rightVideo.duration) / MAX_DURATION;
     draw();
+  });
+  $("#brushRadiusSlider").change(function (e) {
+    brushRadius = parseInt(e.target.value);
+    draw();
+    drawBrush();
   });
 });
