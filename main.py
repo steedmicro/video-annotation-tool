c4a2e0e2082a2f56f250b4499dd5aa6c4b5fca94
steed924 <steed924@outlook.com>
2021-12-09 17:32:47 +0900

    Update Python code - better performance

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ frames = []
 def load():
     file = open("data.out", "rb")
     image_size = height * width
-    frame = file.read(1)
+    frame = file.read(image_size)
     while frame:
         frames.append(frame)
-        frame = file.read(1)
+        frame = file.read(image_size)
 def get_frame(frame_index):
     image = np.zeros((height,width,3), np.uint8)
     for i in range(height):
         for j in range(width):
-            image[i, j] = (0, 0, ord(frames[frame_index * height * width + width * i + j]))
+            image[i, j] = (0, 0, frames[frame_index][width * i + j])
     return image
 def main():
     cv.imshow("window", get_frame(0))
